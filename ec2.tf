f0211f5fe7a5569db9ada4c6e0e7a8d433b638df
TumuVikas <vikast.chowdary@gmail.com>
2024-04-09 21:29:12 +0530

    updated

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "lms-web-server" {
     ami = "ami-0fe2bbc538d630d05"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.lms-web-sn.id
-    key_name = "lms-key1"
+    key_name = "lms-tf"
     security_groups = [aws_security_group.lms-web-sg.id]
     tags = {
         Name = "lms-web-server"
@@ -15,7 +15,7 @@ resource "aws_instance" "lms-api-server" {
     ami = "ami-0fe2bbc538d630d05"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.lms-api-sn.id
-    key_name = "lms-key1"
+    key_name = "lms-tf"
     security_groups = [aws_security_group.lms-api-sg.id]
     tags = {
         Name = "lms-api-server"
@@ -27,7 +27,7 @@ resource "aws_instance" "lms-db-server" {
     ami = "ami-0fe2bbc538d630d05"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.lms-db-sn.id
-    key_name = "lms-key1"
+    key_name = "lms-tf"
     security_groups = [aws_security_group.lms-db-sg.id]
     tags = {
         Name = "lms-db-server"
