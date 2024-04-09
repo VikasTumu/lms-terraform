2f2a29f96dd1076bdfce6c84526db9a57c16ab25
TumuVikas <vikast.chowdary@gmail.com>
2024-04-09 20:22:44 +0530

    update

--- ec2.tf
+++ ec2.tf
@@ -1,24 +1,22 @@
 # Web Server
-resource "aws_instance" "lmds-web-server" {
+resource "aws_instance" "lms-web-server" {
     ami = "ami-010b74bc1a8b29122"
     instance_type = "t3.micro"
     subnet_id = aws_subnet.lms-web-sn.id
     key_name = "lms-key1"
     vpc_security_group_ids = [aws_security_group.lms-web-sg.id]
-    region = "eu-north-1"
     tags = {
         Name = "lms-web-server"
     }
 }
 
 # API Server
-resource "aws_instance" "lmds-api-server" {
+resource "aws_instance" "lms-api-server" {
     ami = "ami-010b74bc1a8b29122"
     instance_type = "t3.micro"
     subnet_id = aws_subnet.lms-api-sn.id
     key_name = "lms-key1"
     vpc_security_group_ids = [aws_security_group.lms-api-sg.id]
-    region = "eu-north-1"
     tags = {
         Name = "lms-api-server"
     }
@@ -31,7 +29,6 @@ resource "aws_instance" "lms-db-server" {
     subnet_id = aws_subnet.lms-db-sn.id
     key_name = "lms-key1"
     vpc_security_group_ids = [aws_security_group.lms-db-sg.id]
-    region = "eu-north-1"
     tags = {
         Name = "lms-db-server"
     }
