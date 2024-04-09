bd01268a3e0cca2407b8ecb268e8de9d5673d318
TumuVikas <vikast.chowdary@gmail.com>
2024-04-09 18:34:52 +0530

    ec2 launching code

---
+++ ec2.tf
@@ -0,0 +1,35 @@
+# Web Server
+resource "aws_instance" "lmds-web-server" {
+    ami = "ami-010b74bc1a8b29122"
+    instance_type = "t3.micro"
+    subnet_id = aws_subnet.lms-web-sn.id
+    key_name = "lms-key1"
+    vpc_security_group_ids = aws_security_group.lms-web-sg.id
+    tags = {
+        Name = "lms-web-server"
+    }
+}
+
+# API Server
+resource "aws_instance" "lmds-api-server" {
+    ami = "ami-010b74bc1a8b29122"
+    instance_type = "t3.micro"
+    subnet_id = aws_subnet.lms-api-sn.id
+    key_name = "lms-key1"
+    vpc_security_group_ids = aws_security_group.lms-api-sg.id
+    tags = {
+        Name = "lms-api-server"
+    }
+}
+
+# DB server
+resource "aws_instance" "lms-db-server" {
+    ami = "ami-010b74bc1a8b29122"
+    instance_type = "t3.micro"
+    subnet_id = aws_subnet.lms-db-sn.id
+    key_name = "lms-key1"
+    vpc_security_group_ids = aws_security_group.lms-db-sg.id
+    tags = {
+        Name = "lms-db-server"
+    }
+}
