5a5a2811603e13c4925d3d67a4ad242ca58cd54a
TumuVikas <vikast.chowdary@gmail.com>
2024-04-10 10:52:53 +0530

    updated

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region = "ap-southeast-2"
-    access_key = "AKIAWQHD2AKWIK7P7YGZ"
-    secret_key = "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"
+    region = "eu-north-1"
+    access_key = "AKIAWQHD2AKWPVZTX6PI"
+    secret_key = "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"
 }
