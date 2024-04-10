df75d405e1f1be1ef6ceadfb7ea80d60a0f29a02
TumuVikas <vikast.chowdary@gmail.com>
2024-04-10 11:06:44 +0530

    updated

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "lms-web-server" {
     ami = "ami-010b74bc1a8b29122"
     instance_type = "t3.micro"
     subnet_id = aws_subnet.lms-web-sn.id
-    key_name = "lms-tf"
+    key_name = "terrform"
     vpc_security_group_ids = [aws_security_group.lms-web-sg.id]
     tags = {
         Name = "lms-web-server"
@@ -15,7 +15,7 @@ resource "aws_instance" "lms-api-server" {
     ami = "ami-010b74bc1a8b29122"
     instance_type = "t3.micro"
     subnet_id = aws_subnet.lms-api-sn.id
-    key_name = "lms-tf"
+    key_name = "terraform"
     vpc_security_group_ids = [aws_security_group.lms-api-sg.id]
     tags = {
         Name = "lms-api-server"
@@ -27,7 +27,7 @@ resource "aws_instance" "lms-db-server" {
     ami = "ami-010b74bc1a8b29122"
     instance_type = "t3.micro"
     subnet_id = aws_subnet.lms-db-sn.id
-    key_name = "lms-tf"
+    key_name = "terraform"
     vpc_security_group_ids = [aws_security_group.lms-db-sg.id]
     tags = {
         Name = "lms-db-server"
